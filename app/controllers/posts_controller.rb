96f99db6e6603c055a9a865d0a87bb3d68422cea
daidai <hsdays81@gmail.com>
2022-09-23 20:05:46 +0900

    -

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,6 @@
 class PostsController < ApplicationController
   def index
-    # viewで使いたい変数をここで定義する。
-    @posts = [
-      "今日からProgateでRailsの勉強するよー！",
-      "投稿一覧ページ作成中！"
-    ]
+    # @postsに代入されている配列をPost.allに書き換えてください
+    @posts = Post.all
   end
 end
