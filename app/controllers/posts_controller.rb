d0da3d9dd341424816e08b8d9ee53d61b5a01c35
daidai <hsdays81@gmail.com>
2022-09-24 13:04:39 +0900

    rails lesson 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,20 @@
 class PostsController < ApplicationController
   def index
-    # @postsに代入されている配列をPost.allに書き換えてください
-    @posts = Post.all
+    # Post.allにorderメソッドを用いて、新しい投稿が上から順に表示されるようにしてください
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def show
+    @id = params[:id]   #ルーティングで「:id」で指定しているurlの番号を取得することができる。
+    @post = Post.find_by(id:params[:id])  #URLの番号に対応するテーブルのidを取得し、postにする
+  end
+
+  def new
+  end
+  def create
+    # フォームから送信されたデータを受け取り、保存する処理を追加してください
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "posts/index" => "posts#index"
-
+  get "posts/index" => "posts#index" #一覧ページ
+  get "posts/new" => "posts#new" #新規作成ページ
+  get "posts/:id" => "posts#show" #投稿ページ、※posts/〇〇すべてを読み込むので注意が必要。下に位置させる
+  post "posts/create" => "posts#create" #データ送信のルーティング
   get "home" => "home#top"
   get "about" => "home#about"
 end
